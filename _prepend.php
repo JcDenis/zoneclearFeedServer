2a8a03621fef3bfc97157ae68d8c8aa05792db41
Jean-Christian Denis <contact@jcdenis.fr>
2021-09-11 12:02:49 +0200

    remove SoCialMe feature as this plugin is dead

--- _prepend.php
+++ _prepend.php
@@ -12,44 +12,26 @@
  */
 
 if (!defined('DC_RC_PATH')) {
-
-    return null;
-}
-
-if ($core->getVersion('zoneclearFeedServer') != 
-    $core->plugins->moduleInfo('zoneclearFeedServer', 'version')) {
-
     return null;
 }
 
 $d = dirname(__FILE__).'/inc/';
 
-$__autoload['zoneclearFeedServer'] = $d.'class.zoneclear.feed.server.php';
-$__autoload['zcfsFeedsList'] = $d.'lib.zcfs.list.php';
-$__autoload['zcfsEntriesList'] = $d.'lib.zcfs.list.php';
-$__autoload['zcfsFeedsActionsPage'] = $d.'class.zcfs.feedsactions.php';
-$__autoload['zcfsDefaultFeedsActions'] = $d.'class.zcfs.feedsactions.php';
+$__autoload['zoneclearFeedServer'] = $d . 'class.zoneclear.feed.server.php';
+$__autoload['zcfsFeedsList'] = $d . 'lib.zcfs.list.php';
+$__autoload['zcfsEntriesList'] = $d . 'lib.zcfs.list.php';
+$__autoload['zcfsFeedsActionsPage'] = $d . 'class.zcfs.feedsactions.php';
+$__autoload['zcfsDefaultFeedsActions'] = $d . 'class.zcfs.feedsactions.php';
 
-# public url for page of description of the flux
+// public url for page of description of the flux
 $core->url->register(
     'zoneclearFeedsPage',
     'zcfeeds',
     '^zcfeeds(.*?)$',
     array('zcfsUrlHandler', 'zcFeedsPage')
 );
-/*
-# Add to plugn soCialMe (writer part)
-$__autoload['zcfsSoCialMeWriter'] = $d.'lib.zcfs.socialmewriter.php';
-$core->addBehavior(
-    'soCialMeWriterMarker',
-    array('zcfsSoCialMeWriter', 'soCialMeWriterMarker')
-);
-$core->addBehavior(
-    'zoneclearFeedServerAfterFeedUpdate',
-    array('zcfsSoCialMeWriter', 'zoneclearFeedServerAfterFeedUpdate')
-);
-//*/
-# Add to report on plugin activityReport
+
+// Add to report on plugin activityReport
 if (defined('ACTIVITY_REPORT')) {
-    require_once $d.'lib.zcfs.activityreport.php';
+    require_once $d  .'lib.zcfs.activityreport.php';
 }
