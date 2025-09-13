1809630cb1fbb2a90d61403e7bbf18c7981fd55d
Jean-Christian Denis <contact@jcdenis.fr>
2025-09-13 21:53:33 +0200

    release 2025.09.13

--- _define.php
+++ _define.php
@@ -19,7 +19,7 @@ $this->registerModule(
     'Feeds server',
     'Mix your blog with a feeds planet',
     'Jean-Christian Denis, BG, Pierre Van Glabeke',
-    '2025.09.11',
+    '2025.09.13',
     [
         'requires'    => [['core', '2.36']],
         'permissions' => 'My',
